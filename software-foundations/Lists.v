72727570dc44e41b356299752f69dc936cb4e023
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-08-26 13:29:19 +0200

    Do dictionary exercises

--- software-foundations/Lists.v
+++ software-foundations/Lists.v
@@ -1096,15 +1096,15 @@ Fixpoint find (key : nat) (d : dictionary) : natoption :=
                        else (find key d')
   end.
 
-
-
 (** **** Exercise: 1 star (dictionary_invariant1) *)
 (** Complete the following proof. *)
 
 Theorem dictionary_invariant1' : forall (d : dictionary) (k v: nat),
   (find k (insert k v d)) = Some v.
 Proof.
- (* FILL IN HERE *) Admitted.
+  intros d k v.
+  simpl. rewrite <- beq_nat_refl. reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 1 star (dictionary_invariant2) *)
@@ -1113,10 +1113,10 @@ Proof.
 Theorem dictionary_invariant2' : forall (d : dictionary) (m n o: nat),
   beq_nat m n = false -> find m d = find m (insert n o d).
 Proof.
- (* FILL IN HERE *) Admitted.
+  intros d m n o H.
+  simpl. rewrite H. reflexivity.
+Qed.
 (** [] *)
-
-
 
 End Dictionary.
 
